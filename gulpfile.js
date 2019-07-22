d9799e065447478de781cedae3aa109e09bfa58d
Yongmin Kim <yongmin86k@gmail.com>
2019-07-22 11:29:10 -0700

    add default task with terser and gulp rename

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp"); // Load Gulp!
+// Now that we've installed the terser package we can require it:
+const terser = require("gulp-terser");
+const rename = require("gulp-rename");
+
+gulp.task("default", function() {
+  return gulp
+    .src("./js/*.js") // What files do we want gulp to consume?
+    .pipe(terser()) // Call the terser function on these files
+    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
+    .pipe(gulp.dest("./build/js")); // Where do we put the result?
+});
---
+++ js/main.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+
+function sayHello() { 
+    return "Hello World"
+ }
