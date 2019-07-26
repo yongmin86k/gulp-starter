5d3a32e66938e8cd66c1740078efaa9b3b73e5d7
Yongmin Kim <yongmin86k@gmail.com>
2019-07-26 11:31:09 -0700

    add gulp-sass, gulp-prettyerror

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,15 @@ const gulp = require("gulp"),
   rename = require("gulp-rename"),
   browserSync = require("browser-sync"),
   cssnano = require("gulp-cssnano"),
+  sass = require('gulp-sass'),
+  prettyError = require('gulp-prettyerror'),
   autoprefixer = require("gulp-autoprefixer"),
   eslint = require('gulp-eslint');
-
 
 gulp.task("watch", function() {
   gulp.watch("js/*.js", gulp.series("scripts"));
   gulp.watch("css/*.css", gulp.series("styles"));
+  gulp.watch("sass/*.scss", gulp.series("sass"));
   gulp.watch("*.html").on("change", browserSync.reload);
 });
 
@@ -33,6 +35,22 @@ gulp.task('scripts',
                 .pipe(gulp.dest("./build/js"));
         })
 );
+
+gulp.task('sass', function() {
+  return gulp
+    .src('./sass/style.scss')
+    .pipe(prettyError())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions'],
+      }),
+    )
+    .pipe(gulp.dest('./build/css'))
+    .pipe(cssnano())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('./build/css'));
+});
 
 gulp.task("styles", function() {
   return gulp
